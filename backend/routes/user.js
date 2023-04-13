b2a09cd454a5642be3e6579e19d940a973026cf5
William Baele <baelewilliam1999@gmail.com>
2023-04-13 14:34:46 +0200

    added route

---
+++ backend/routes/user.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const router = express.Router();
+
+// Login Route
+router.post('/login', () => {})
+
+// Signup Route
+router.post('/signup', () => {})
+
+
+module.exports = router
