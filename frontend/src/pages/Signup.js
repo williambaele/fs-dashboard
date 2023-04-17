827e86a2eb0cae6bd5028dcecde2248e297185ec
William Baele <baelewilliam1999@gmail.com>
2023-04-17 10:31:21 +0200

    some adds

---
+++ frontend/src/pages/Signup.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const Signup = () => {
+
+  const [email, setEmail] = useState("")
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default Signup;
