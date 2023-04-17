6ef34a8eae94169ad066f4fe7ab181c0b427e6aa
William Baele <baelewilliam1999@gmail.com>
2023-04-17 12:03:28 +0200

    some adds

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
+import { useLogin } from '../hooks/useLogin';
 
 const Login = () => {
 
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
 
+  const {login, error, isLoading} = useLogin()
+
   const handleSubmit = async (e) => {
     e.preventDefault()
-    console.log(email, password)
+    await login(email, password)
   }
 
   return (
@@ -16,7 +19,9 @@ const Login = () => {
         <h2>Login</h2>
         <input type="text" placeholder='email' onChange={(e) => setEmail(e.target.value)} value={email}/>
         <input type="password" placeholder='password' onChange={(e) => setPassword(e.target.value)} value={password}/>
-        <button>Login</button>
+        <button disabled={isLoading}>Signup</button>
+        { error &&
+        <p>{error}</p>}
       </form>
 
     </div>
