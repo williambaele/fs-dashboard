6e9f55af117b470c68d563b669781841e9995ce1
William Baele <baelewilliam1999@gmail.com>
2023-04-17 11:12:49 +0200

    some adds

---
+++ frontend/src/hooks/useSignup.js
@@ -0,0 +1,11 @@
+import { useAuthContext } from './useAuthContext'
+import { useState } from 'react'
+
+
+export const useSignup = () => {
+
+  const [error, setError] = useState(null)
+  const [isLoading, setIsLoading] = useState(null)
+
+
+}
