a39c47adb4f2b9b5c60a86942909deb77f3addb1
William Baele <baelewilliam1999@gmail.com>
2023-04-17 13:24:06 +0200

    some adds

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { useAuthContext } from './hooks/useAuthContext'
 
 // pages & components
 import Signup from './pages/Signup'
@@ -8,6 +9,8 @@ import Home from './pages/Home'
 import Navbar from './components/Navbar'
 
 function App() {
+
+  const {user} = useAuthContext()
   return (
     <div className="App">
       <BrowserRouter>
@@ -16,15 +19,15 @@ function App() {
           <Routes>
             <Route
               path="/"
-              element={<Home />}
+              element={user ? <Home/> : <Navigate to ="/login"/>}
             />
             <Route
               path="/signup"
-              element={<Signup />}
+              element={!user ? <Signup /> : <Navigate to="/"/>}
             />
             <Route
               path="/login"
-              element={<Login />}
+              element={!user ? <Login /> : <Navigate to="/"/>}
             />
           </Routes>
         </div>
