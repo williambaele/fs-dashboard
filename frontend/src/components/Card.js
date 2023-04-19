617347a21ff1a90d0ae5f930cfcc084f38b6bc60
William Baele <baelewilliam1999@gmail.com>
2023-04-19 14:37:39 +0200

    fixed

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,123 +1,40 @@
 import React from "react";
-import { useItemsContext } from "../hooks/useItemsContext";
-import { useAuthContext } from "../hooks/useAuthContext";
+
 import { Link } from "react-router-dom";
 
 const Card = ({ item }) => {
-  const { dispatch } = useItemsContext();
-  const { user } = useAuthContext();
-
-  const handleClick = async () => {
-    if (!user) {
-      return;
-    }
-
-    const response = await fetch("/api/items/" + item._id, {
-      method: "DELETE",
-      headers: {
-        Authorization: `Bearer ${user.token}`,
-      },
-    });
-    const json = await response.json();
-
-    if (response.ok) {
-      dispatch({ type: "DELETE_ITEM", payload: json });
-    }
-  };
   return (
     <div>
       <Link to={`/item/${item._id}`}>
-        <div class="relative flex w-full max-w-xs flex-col overflow-hidden rounded-md border border-gray-100 bg-white shadow-md">
-          <div class="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl">
+        <div class="relative flex w-full flex-col p-4 overflow-hidden rounded-md border border-gray-100 bg-white shadow-md">
+          <div class="relative flex h-60 w-full rounded-xl">
             <img
-            alt={item.title}
-              className="object-cover"
+              alt={item.title}
+              className="object-cover rounded-md"
               src="https://images.unsplash.com/photo-1600185365483-26d7a4cc7519?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c25lYWtlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
             />
           </div>
           <div class="mt-4 px-5 pb-5">
-            <div>
-              <h5 class="text-xl tracking-tight text-slate-900">
-                {item.title}
-              </h5>
+            <div className="flex justify-between mb-4">
+              <h5 class="text-xl text-slate-900">{item.title}</h5>
+              <span class="text-xl font-bold text-slate-900">
+                {item.price}€
+              </span>
             </div>
-            <div class="mt-2 mb-5 flex items-center justify-between">
-              <p>
-                <span class="text-xl font-bold text-slate-900">
-                  {item.price}€
-                </span>
-              </p>
-              <div class="flex items-center">
-                <svg
-                  aria-hidden="true"
-                  class="h-5 w-5 text-yellow-300"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                </svg>
-                <svg
-                  aria-hidden="true"
-                  class="h-5 w-5 text-yellow-300"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                </svg>
-                <svg
-                  aria-hidden="true"
-                  class="h-5 w-5 text-yellow-300"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                </svg>
-                <svg
-                  aria-hidden="true"
-                  class="h-5 w-5 text-yellow-300"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                </svg>
-                <svg
-                  aria-hidden="true"
-                  class="h-5 w-5 text-yellow-300"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                </svg>
-                <span class="mr-2 ml-3 rounded bg-yellow-200 px-2.5 py-0.5 text-xs font-semibold">
-                  5.0
-                </span>
+            <div className="flex justify-between">
+              <div className="flex gap-2 items-center">
+                <img
+                  src="https://www.booska-p.com/wp-content/uploads/2022/10/hasbulla.jpg"
+                  alt=""
+                  className="rounded-full h-6 w-6 object-cover"
+                />
+                <span class="text-md font-bold text-slate-900">Luca2001</span>
+              </div>
+              <div className="flex gap-1 items-center">
+                <span class="text-md font-bold text-slate-900">4.5/5</span>
+                <span class="text-md font-bold text-slate-900">(7)</span>
               </div>
             </div>
-            <div class="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="mr-2 h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                stroke-width="2"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                />
-              </svg>
-              Add to cart
-            </div>
-            <span className="material-symbols-outlined" onClick={handleClick}>
-              delete
-            </span>
           </div>
         </div>
       </Link>
--- frontend/src/pages/Items.js
+++ frontend/src/pages/Items.js
@@ -28,12 +28,12 @@ const Items = () => {
   return (
     <div>
       <Navbar />
-      <div className="min-h-screen gap-4 container mx-auto">
+      <div className="min-h-screen container mx-auto">
         <div className="grid md:grid-cols-6">
           <div className="md:col-span-1 col-span-6 bg-purple-400 md:min-h-screen">a</div>
           <div className="md:col-span-5 col-span-6 bg-yellow-400">
             <div className="bg-red-400 h-20"></div>
-            <div className="grid md:grid-cols-3 p-2">
+            <div className="grid md:grid-cols-3 p-2 gap-4">
               {items &&
                 items.map((item) => <Card key={item._id} item={item} />)}
             </div>
