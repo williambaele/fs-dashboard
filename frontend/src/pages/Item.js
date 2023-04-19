1d8dc7439abb289d8eb207fe04ab84dc59889ec3
William Baele <baelewilliam1999@gmail.com>
2023-04-19 12:08:07 +0200

    adds

--- frontend/src/pages/Item.js
+++ frontend/src/pages/Item.js
@@ -1,15 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Item = () => {
   const { id } = useParams();
+  const [item, setItem] = useState(null);
+  const { user } = useAuthContext();
+
+
+  useEffect(() => {
+    const fetchItem = async () => {
+      const response = await fetch(`/api/items/${id}`, {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+      const json = await response.json();
+
+      if (response.ok) {
+        setItem(json);
+      }
+    };
+
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    if (user) {
+      fetchItem();
+    }
+  }, [id, user.token]);
 
   return (
     <div>
       <Navbar />
       <div className="mx-auto container bg-red-200">
-        <p>Item ID: {id}</p>
+        {item ? (
+          <div>
+            <p>Item ID: {id}</p>
+            <p>Name: {item.title}</p>
+            <p>Size: {item.size}</p>
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
       </div>
     </div>
   );
