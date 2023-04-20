fd89865211ca2707ccc6b26bb8ba80cba4130b59
William Baele <baelewilliam1999@gmail.com>
2023-04-20 12:23:32 +0200

    deleted the user logged in restriction on items

--- backend/routes/items.js
+++ backend/routes/items.js
@@ -1,3 +1,35 @@
+// const express = require('express')
+// const {
+//   createItem,
+//   getItems,
+//   getItem,
+//   deleteItem,
+//   updateItem
+// } = require('../controllers/itemController')
+// const requireAuth = require('../middleware/requireAuth')
+
+// const router = express.Router()
+
+// // require auth for all workout routes
+// router.use(requireAuth)
+
+// // GET all workouts
+// router.get('/', getItems)
+
+// //GET a single workout
+// router.get('/:id', getItem)
+
+// // POST a new workout
+// router.post('/', createItem)
+
+// // DELETE a workout
+// router.delete('/:id', deleteItem)
+
+// // UPDATE a workout
+// router.patch('/:id', updateItem)
+
+
+// module.exports = router
 const express = require('express')
 const {
   createItem,
@@ -6,27 +38,17 @@ const {
   deleteItem,
   updateItem
 } = require('../controllers/itemController')
-// const requireAuth = require('../middleware/requireAuth')
+const requireAuth = require('../middleware/requireAuth')
 
 const router = express.Router()
 
-// require auth for all workout routes
-// router.use(requireAuth)
+// require auth only for create, update, and delete routes
+router.post('/', requireAuth, createItem)
+router.delete('/:id', requireAuth, deleteItem)
+router.patch('/:id', requireAuth, updateItem)
 
-// GET all workouts
+// GET routes don't require auth
 router.get('/', getItems)
-
-//GET a single workout
 router.get('/:id', getItem)
-
-// POST a new workout
-router.post('/', createItem)
-
-// DELETE a workout
-router.delete('/:id', deleteItem)
-
-// UPDATE a workout
-router.patch('/:id', updateItem)
-
 
 module.exports = router
