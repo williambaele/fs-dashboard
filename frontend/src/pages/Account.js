608561413dc4bacbc17ac24d3f18993065f2948f
William Baele <baelewilliam1999@gmail.com>
2023-04-20 13:46:19 +0200

    deleted the user logged in restriction on items

--- frontend/src/pages/Account.js
+++ frontend/src/pages/Account.js
@@ -10,7 +10,7 @@ const Account = () => {
     <div>
       <Navbar />
       <div className="min-h-screen mx-auto container">
-        <div>
+        <div className="py-10">
           <form class="space-y-8 divide-y divide-gray-200">
             <div class="space-y-8 divide-y divide-gray-200 sm:space-y-5">
               <div>
@@ -30,8 +30,7 @@ const Account = () => {
                       for="username"
                       class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
                     >
-                      {" "}
-                      {user && user.email}{" "}
+                      {user && user.email}
                     </label>
                     <div class="mt-1 sm:mt-0 sm:col-span-2">
                       <div class="max-w-lg flex rounded-md shadow-sm">
@@ -105,8 +104,7 @@ const Account = () => {
                       for="cover-photo"
                       class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
                     >
-                      {" "}
-                      Cover photo{" "}
+                      Cover photo
                     </label>
                     <div class="mt-1 sm:mt-0 sm:col-span-2">
                       <div class="max-w-lg flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
@@ -165,8 +163,7 @@ const Account = () => {
                       for="first-name"
                       class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
                     >
-                      {" "}
-                      First name{" "}
+                      First name
                     </label>
                     <div class="mt-1 sm:mt-0 sm:col-span-2">
                       <input
@@ -184,8 +181,7 @@ const Account = () => {
                       for="last-name"
                       class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
                     >
-                      {" "}
-                      Last name{" "}
+                      Last name
                     </label>
                     <div class="mt-1 sm:mt-0 sm:col-span-2">
                       <input
@@ -203,285 +199,17 @@ const Account = () => {
                       for="email"
                       class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
                     >
-                      {" "}
-                      Email address{" "}
+                      Email address
                     </label>
                     <div class="mt-1 sm:mt-0 sm:col-span-2">
                       <input
                         id="email"
                         name="email"
                         type="email"
+                        placeholder={user && user.email}
                         autocomplete="email"
                         class="block max-w-lg w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md"
                       />
-                    </div>
-                  </div>
-
-                  <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                    <label
-                      for="country"
-                      class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
-                    >
-                      {" "}
-                      Country{" "}
-                    </label>
-                    <div class="mt-1 sm:mt-0 sm:col-span-2">
-                      <select
-                        id="country"
-                        name="country"
-                        autocomplete="country-name"
-                        class="max-w-lg block focus:ring-indigo-500 focus:border-indigo-500 w-full shadow-sm sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
-                      >
-                        <option>United States</option>
-                        <option>Canada</option>
-                        <option>Mexico</option>
-                      </select>
-                    </div>
-                  </div>
-
-                  <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                    <label
-                      for="street-address"
-                      class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
-                    >
-                      {" "}
-                      Street address{" "}
-                    </label>
-                    <div class="mt-1 sm:mt-0 sm:col-span-2">
-                      <input
-                        type="text"
-                        name="street-address"
-                        id="street-address"
-                        autocomplete="street-address"
-                        class="block max-w-lg w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md"
-                      />
-                    </div>
-                  </div>
-
-                  <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                    <label
-                      for="city"
-                      class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
-                    >
-                      {" "}
-                      City{" "}
-                    </label>
-                    <div class="mt-1 sm:mt-0 sm:col-span-2">
-                      <input
-                        type="text"
-                        name="city"
-                        id="city"
-                        autocomplete="address-level2"
-                        class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
-                      />
-                    </div>
-                  </div>
-
-                  <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                    <label
-                      for="region"
-                      class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
-                    >
-                      {" "}
-                      State / Province{" "}
-                    </label>
-                    <div class="mt-1 sm:mt-0 sm:col-span-2">
-                      <input
-                        type="text"
-                        name="region"
-                        id="region"
-                        autocomplete="address-level1"
-                        class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
-                      />
-                    </div>
-                  </div>
-
-                  <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                    <label
-                      for="postal-code"
-                      class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
-                    >
-                      {" "}
-                      ZIP / Postal code{" "}
-                    </label>
-                    <div class="mt-1 sm:mt-0 sm:col-span-2">
-                      <input
-                        type="text"
-                        name="postal-code"
-                        id="postal-code"
-                        autocomplete="postal-code"
-                        class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
-                      />
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div class="divide-y divide-gray-200 pt-8 space-y-6 sm:pt-10 sm:space-y-5">
-                <div>
-                  <h3 class="text-lg leading-6 font-medium text-gray-900">
-                    Notifications
-                  </h3>
-                  <p class="mt-1 max-w-2xl text-sm text-gray-500">
-                    We'll always let you know about important changes, but you
-                    pick what else you want to hear about.
-                  </p>
-                </div>
-                <div class="space-y-6 sm:space-y-5 divide-y divide-gray-200">
-                  <div class="pt-6 sm:pt-5">
-                    <div role="group" aria-labelledby="label-email">
-                      <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-baseline">
-                        <div>
-                          <div
-                            class="text-base font-medium text-gray-900 sm:text-sm sm:text-gray-700"
-                            id="label-email"
-                          >
-                            By Email
-                          </div>
-                        </div>
-                        <div class="mt-4 sm:mt-0 sm:col-span-2">
-                          <div class="max-w-lg space-y-4">
-                            <div class="relative flex items-start">
-                              <div class="flex items-center h-5">
-                                <input
-                                  id="comments"
-                                  name="comments"
-                                  type="checkbox"
-                                  class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
-                                />
-                              </div>
-                              <div class="ml-3 text-sm">
-                                <label
-                                  for="comments"
-                                  class="font-medium text-gray-700"
-                                >
-                                  Comments
-                                </label>
-                                <p class="text-gray-500">
-                                  Get notified when someones posts a comment on
-                                  a posting.
-                                </p>
-                              </div>
-                            </div>
-                            <div>
-                              <div class="relative flex items-start">
-                                <div class="flex items-center h-5">
-                                  <input
-                                    id="candidates"
-                                    name="candidates"
-                                    type="checkbox"
-                                    class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
-                                  />
-                                </div>
-                                <div class="ml-3 text-sm">
-                                  <label
-                                    for="candidates"
-                                    class="font-medium text-gray-700"
-                                  >
-                                    Candidates
-                                  </label>
-                                  <p class="text-gray-500">
-                                    Get notified when a candidate applies for a
-                                    job.
-                                  </p>
-                                </div>
-                              </div>
-                            </div>
-                            <div>
-                              <div class="relative flex items-start">
-                                <div class="flex items-center h-5">
-                                  <input
-                                    id="offers"
-                                    name="offers"
-                                    type="checkbox"
-                                    class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
-                                  />
-                                </div>
-                                <div class="ml-3 text-sm">
-                                  <label
-                                    for="offers"
-                                    class="font-medium text-gray-700"
-                                  >
-                                    Offers
-                                  </label>
-                                  <p class="text-gray-500">
-                                    Get notified when a candidate accepts or
-                                    rejects an offer.
-                                  </p>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="pt-6 sm:pt-5">
-                    <div role="group" aria-labelledby="label-notifications">
-                      <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-baseline">
-                        <div>
-                          <div
-                            class="text-base font-medium text-gray-900 sm:text-sm sm:text-gray-700"
-                            id="label-notifications"
-                          >
-                            Push Notifications
-                          </div>
-                        </div>
-                        <div class="sm:col-span-2">
-                          <div class="max-w-lg">
-                            <p class="text-sm text-gray-500">
-                              These are delivered via SMS to your mobile phone.
-                            </p>
-                            <div class="mt-4 space-y-4">
-                              <div class="flex items-center">
-                                <input
-                                  id="push-everything"
-                                  name="push-notifications"
-                                  type="radio"
-                                  class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
-                                />
-                                <label
-                                  for="push-everything"
-                                  class="ml-3 block text-sm font-medium text-gray-700"
-                                >
-                                  {" "}
-                                  Everything{" "}
-                                </label>
-                              </div>
-                              <div class="flex items-center">
-                                <input
-                                  id="push-email"
-                                  name="push-notifications"
-                                  type="radio"
-                                  class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
-                                />
-                                <label
-                                  for="push-email"
-                                  class="ml-3 block text-sm font-medium text-gray-700"
-                                >
-                                  {" "}
-                                  Same as email{" "}
-                                </label>
-                              </div>
-                              <div class="flex items-center">
-                                <input
-                                  id="push-nothing"
-                                  name="push-notifications"
-                                  type="radio"
-                                  class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
-                                />
-                                <label
-                                  for="push-nothing"
-                                  class="ml-3 block text-sm font-medium text-gray-700"
-                                >
-                                  {" "}
-                                  No push notifications{" "}
-                                </label>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
                     </div>
                   </div>
                 </div>
