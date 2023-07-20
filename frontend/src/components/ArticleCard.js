c75797269ee23239cbdf5b2436911ac99d9a7c60
William Baele <baelewilliam1999@gmail.com>
2023-07-20 17:24:23 +0200

    created article's card

--- frontend/src/components/ArticleCard.js
+++ frontend/src/components/ArticleCard.js
@@ -2,18 +2,29 @@ import React from "react";
 
 const ArticleCard = ({ article }) => {
   return (
-    <div className="grid">
-      <div className="relative">
+    <div className="grid w-full">
+      <div className="relative w-full">
         <img
           src="https://ncmaz-nextjs.vercel.app/_next/image?url=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F2662116%2Fpexels-photo-2662116.jpeg&w=2048&q=75"
           alt="hero"
-          className="rounded-t-2xl h-48"
+          className="rounded-t-2xl h-48 bg-cover w-full"
         />
         <p className="absolute top-4 left-4 bg-purple-200 text-purple-500 px-2 text-md rounded-lg">
           Travel
         </p>
       </div>
-      <div className="rounded-b-2xl bg-white p-4">
+      <div className="rounded-b-2xl bg-white p-4 grid">
+        <div className="flex justify-between">
+          <div className="flex items-center gap-4">
+            <img
+              src="https://ncmaz-nextjs.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F3.054bee42.jpg&w=256&q=75"
+              alt=""
+              className="rounded-full h-8"
+            />
+            <p className="text-gray-900 text-md font-bold">John Doe</p>
+          </div>
+          <p className="text-gray-300 text-sm">09/03/2023</p>
+        </div>
         <p>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. In, qui?
         </p>
--- frontend/src/pages/Articles.js
+++ frontend/src/pages/Articles.js
@@ -31,7 +31,7 @@ const Articles = () => {
     <div>
       <Navbar />
       <div className="bg-[#F0F0F0] h-screen">
-        <div className="container mx-auto py-10 grid grid-cols-4 gap-4" >
+        <div className="container mx-auto py-10 grid md:grid-cols-4 gap-4 w-full px-4 md:px-0" >
           {articles.map((article) => (
             <ArticleCard key={article._id} article={article} />
           ))}
