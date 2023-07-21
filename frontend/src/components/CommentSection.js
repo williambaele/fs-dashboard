bf665aa1626bccce69d8ef0c0911b996ff559f38
William Baele <baelewilliam1999@gmail.com>
2023-07-21 17:02:00 +0200

    Created comment section

--- frontend/src/components/CommentSection.js
+++ frontend/src/components/CommentSection.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 const CommentSection = () => {
   return (
-    <div className="border-b border-gray-400 px-4 md:px-0 w-full md:w-1/2 mx-auto container">
-
+    <div className="border-t pt-10 border-gray-400 px-4 md:px-0 w-full md:w-1/2 mx-auto container grid">
+      <h3 className="text-gray-900 font-bold text-3xl">Comments (10)</h3>
+      <div className="grid gap-4 w-full">
+        <form className="space-y-4 pt-6">
+          <textarea className="bg-white shadow-sm rounded-2xl w-full p-2" placeholder="Add to discussion" rows="5"/>
+          <button className="bg-[#4338ca] px-6 py-3 text-white rounded-2xl">Submit</button>
+        </form>
+      </div>
     </div>
   );
 };
