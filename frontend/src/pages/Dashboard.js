5e83dc2974a88afd15c4d31c2b2d3b70e22a6806
William Baele <baelewilliam1999@gmail.com>
2023-07-24 13:57:25 +0200

    better style for article card

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -8,10 +8,25 @@ const Dashboard = () => {
   const { user } = useAuthContext();
   console.log({ user });
 
+  const menu = [
+    {
+      name: "New article",
+      emoji: "📝",
+    },
+    {
+      name: "Your posts",
+      emoji: "📖",
+    },
+    {
+      name: "Edit profile",
+      emoji: "⚙️",
+    }
+  ];
+
   return (
     <>
       <Navbar />
-      <div className="bg-[#F0F0F0] h-full">
+      <div className="bg-[#F0F0F0]">
         <div className="mx-auto container px-4 md:px-0 py-10">
           <div className="bg-white p-8 rounded-2xl">
             <div className="space-y-4 py-4">
@@ -23,8 +38,15 @@ const Dashboard = () => {
                 and profile.
               </p>
             </div>
-            <div className="grid md:grid-cols-12 h-screen">
-              <div className="md:col-span-3 bg-red-300"></div>
+            <div className="grid md:grid-cols-12 h-full gap-4">
+              <div className="md:col-span-3 grid gap-2">
+                {menu.map((item) => (
+                  <div className="flex gap-2 hover:bg-gray-100 px-4 py-2 rounded-xl cursor-pointer">
+                    <p className="text-xl">{item.emoji}</p>
+                    <p className="text-xl">{item.name}</p>
+                  </div>
+                ))}
+              </div>
               <div className="md:col-span-9 bg-purple-300"></div>
             </div>
           </div>
