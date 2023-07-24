d4b3f7131523b14d35a4a626914d812102e0ba48
William Baele <baelewilliam1999@gmail.com>
2023-07-24 13:48:37 +0200

    better style for article card

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -13,10 +13,20 @@ const Dashboard = () => {
       <Navbar />
       <div className="bg-[#F0F0F0] h-full">
         <div className="mx-auto container px-4 md:px-0 py-10">
-          <h1 className="text-4xl font-bold text-gray-900 text-center">Welcome to your dashboard</h1>
-          <div className="grid md:grid-cols-12 h-screen">
-            <div className="md:col-span-3 bg-red-300"></div>
-            <div className="md:col-span-9 bg-purple-300"></div>
+          <div className="bg-white p-8 rounded-2xl">
+            <div className="space-y-4 py-4">
+              <h1 className="text-5xl font-bold text-gray-900 text-center">
+                Dashboard
+              </h1>
+              <p className="text-gray-400 text-lg text-center">
+                View your dashboard, manage your posts, subscription and edit
+                and profile.
+              </p>
+            </div>
+            <div className="grid md:grid-cols-12 h-screen">
+              <div className="md:col-span-3 bg-red-300"></div>
+              <div className="md:col-span-9 bg-purple-300"></div>
+            </div>
           </div>
         </div>
       </div>
