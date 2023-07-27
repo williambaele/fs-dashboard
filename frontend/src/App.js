2d448ca444841af6e8c7eda019d38439d74ec7a2
William Baele <baelewilliam1999@gmail.com>
2023-07-27 14:03:23 +0200

    add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import Article from "./pages/Article";
 
 function App() {
   const { user } = useAuthContext();
+  console.log(user)
 
   return (
     <div className="App">
--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -1,37 +1,46 @@
 import { useAuthContext } from './useAuthContext'
 import { useState } from 'react'
 
-
 export const useLogin = () => {
-
   const [error, setError] = useState(null)
-  const [isLoading, setIsLoading] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
 
-  const {dispatch} = useAuthContext()
+  const { dispatch } = useAuthContext()
 
   const login = async (email, password) => {
     setIsLoading(true)
     setError(null)
 
-    const response = await fetch('/api/user/login', {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({email, password})
-    })
-    const json = await response.json()
+    try {
+      const response = await fetch('/api/user/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password })
+      })
 
-    if (!response.ok) {
+      if (!response.ok) {
+        const json = await response.json()
+        setIsLoading(false)
+        setError(json.error)
+      } else {
+        const json = await response.json()
+
+        // Extract the user pseudo from the response data
+        const { pseudo } = json;
+
+        // Update the user context with the received data, including the pseudo
+        dispatch({ type: 'LOGIN', payload: { ...json, pseudo } })
+
+        // Save the user data to local storage
+        localStorage.setItem('user', JSON.stringify(json))
+
+        setIsLoading(false)
+      }
+    } catch (error) {
       setIsLoading(false)
-      setError(json.error)
-    }
-    if (response.ok) {
-      localStorage.setItem('user', JSON.stringify(json))
-
-      dispatch({type: 'LOGIN', payload: json})
-
-      setIsLoading(false)
+      setError('An error occurred during login')
     }
   }
 
-  return { login, isLoading, error}
+  return { login, isLoading, error }
 }
