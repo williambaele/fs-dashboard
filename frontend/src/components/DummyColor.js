e94e905aaf8fb255b7be3872d8c910555b6022ad
William Baele <baelewilliam1999@gmail.com>
2023-08-03 11:14:53 +0200

    fixed tailwind colors

---
+++ frontend/src/components/DummyColor.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const DummyColor = () => {
+  return (
+    <div>
+      <p className="bg-orange-300 text-orange-500">O</p>
+      <p className="bg-purple-300 text-purple-500">O</p>
+      <p className="bg-blue-300 text-blue-500">O</p>
+      <p className="bg-green-300 text-green-500">O</p>
+    </div>
+  );
+};
+
+export default DummyColor;
