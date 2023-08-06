9e270635b4be5b94254e65beaab92304191a0889
William Baele <baelewilliam1999@gmail.com>
2023-08-06 11:43:41 +0200

    Added comment model & controller

---
+++ backend/models/commentModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Comment", commentSchema);
--- frontend/src/components/HeroSlider.js
+++ frontend/src/components/HeroSlider.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import ArticleStats from "./ArticleStats";
 import { Link } from "react-router-dom";
 
