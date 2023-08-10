b9f1c29f0be9845f16f94b23761049813ab32e21
William Baele <baelewilliam1999@gmail.com>
2023-08-10 10:41:04 +0200

    fixed added components

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,21 +2,19 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import TrendingTopics from "../components/TrendingTopics";
-// import HeroBanner from "../components/HeroBanner";
 import TrendingAuthors from "../TrendingAuthors";
 import LatestArticles from "../components/LatestArticles";
 import HeroSlider from "../components/HeroSlider";
 
-const Home = ({articles}) => {
+const Home = ({ articles }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <div className="flex-grow pt-16 bg-[#e5e7eb]">
-        {/* <HeroBanner /> */}
-        <HeroSlider articles={articles}/>
+        <HeroSlider articles={articles} />
         <TrendingTopics />
-        <TrendingAuthors/>
-        <LatestArticles articles={articles}/>
+        <TrendingAuthors />
+        <LatestArticles articles={articles} />
       </div>
       <Footer />
     </div>
