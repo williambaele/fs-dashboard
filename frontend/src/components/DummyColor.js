5bec11db8d796abd677f752851391e80c37711e2
William Baele <baelewilliam1999@gmail.com>
2023-08-19 12:38:54 +0200

    Added label colorized

---
+++ frontend/src/components/DummyColor.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const DummyColor = () => {
+  return (
+    <div>
+      <p className="text-red-800 bg-red-500">Hello world </p>
+      <p className="text-green-800 bg-green-500">Hello world </p>
+      <p className="text-yellow-800 bg-yellow-500">Hello world </p>
+
+    </div>
+  );
+};
+
+export default DummyColor;
--- frontend/src/components/TasksTableRow.js
+++ frontend/src/components/TasksTableRow.js
@@ -1,11 +1,11 @@
 import React from "react";
-
-const TasksTableRow = ({task}) => {
+import TaskStatusLabel from "./TaskStatusLabel";
+const TasksTableRow = ({ task }) => {
   const createdAtDate = new Date(task.createdAt);
-  const formattedDate = createdAtDate.toLocaleDateString('fr-FR', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
+  const formattedDate = createdAtDate.toLocaleDateString("fr-FR", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
   });
   return (
     <tr>
@@ -22,9 +22,7 @@ const TasksTableRow = ({task}) => {
       </td>
       <td class="h-px w-40 whitespace-nowrap">
         <div class="px-6 py-3">
-          <span class="block text-sm text-gray-500">
-            {task.description}
-          </span>
+          <span class="block text-sm text-gray-500">{task.description}</span>
         </div>
       </td>
       <td class="h-px w-20 whitespace-nowrap">
@@ -36,23 +34,7 @@ const TasksTableRow = ({task}) => {
           />
         </div>
       </td>
-      <td class="h-px w-px whitespace-nowrap">
-        <div class="px-6 py-3">
-          <span class="inline-flex items-center gap-1.5 py-0.5 px-2 rounded-full text-xs font-medium bg-[#FBE654] text-orange-800">
-            <svg
-              class="w-2.5 h-2.5"
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              viewBox="0 0 16 16"
-            >
-              <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-            </svg>
-            Warning
-          </span>
-        </div>
-      </td>
+      <TaskStatusLabel status={task.taskLevel}/>
       <td class="h-px w-32 whitespace-nowrap">
         <div class="px-6 py-3">
           <div class="flex items-center gap-x-3">
