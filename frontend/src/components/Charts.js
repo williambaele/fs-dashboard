453781bdcbd3c748aca045cae0e1be8aa1a9b2a9
William Baele <baelewilliam1999@gmail.com>
2023-08-21 16:44:46 +0200

    Added google charts

--- frontend/src/components/Charts.js
+++ frontend/src/components/Charts.js
@@ -1,26 +1,21 @@
 import React from "react";
-import ReactEcharts from "echarts-for-react";
+import { Chart } from "react-google-charts";
 
 const Charts = () => {
-  const option = {
-    xAxis: {
-      type: "category",
-      data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-    },
-    yAxis: {
-      type: "value",
-    },
-    series: [
-      {
-        data: [120, 200, 150, 80, 70, 110, 130],
-        type: "bar",
-      },
-    ],
-  };
   return (
     <div className=" bg-[#232323] p-4 rounded h-full">
       <h2 className="text-2xl text-gray-300">Your created tasks</h2>
-      <ReactEcharts option={option} />
+      <Chart
+        chartType="ScatterChart"
+        data={[
+          ["Age", "Weight"],
+          [4, 5.5],
+          [8, 12],
+        ]}
+        width="100%"
+        height="400px"
+        legendToggle
+      />
     </div>
   );
 };
--- frontend/src/components/HelpCard.js
+++ frontend/src/components/HelpCard.js
@@ -2,8 +2,10 @@ import React from "react";
 
 const HelpCard = () => {
   return (
-    <div className="
-    bg-[#593EFE] p-4 rounded h-full space-y-4">
+    <div
+      className="
+    bg-[#593EFE] p-4 rounded h-full space-y-4"
+    >
       <h2 className="text-white">Need help ?</h2>
     </div>
   );
