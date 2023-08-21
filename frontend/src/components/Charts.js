03ba6fdc6f08bbb0326dfabdfa4ee58d7f90dc69
William Baele <baelewilliam1999@gmail.com>
2023-08-21 17:50:21 +0200

    Added google charts

--- frontend/src/components/Charts.js
+++ frontend/src/components/Charts.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Chart } from "react-google-charts";
 
 const Charts = () => {
@@ -16,39 +16,44 @@ const Charts = () => {
       subtitle: "Sales, Expenses, and Profit: 2014-2017",
     },
   };
+
+  const [activePeriod, setActivePeriod] = useState("month");
   return (
     <div className="bg-[#232323] p-4 rounded h-full flex flex-col gap-6">
       <div className="flex justify-between">
         <h2 className="text-2xl text-gray-300">Your created tasks</h2>
-        <div className="flex gap-4">
-          <div class="cursor-pointer py-2 px-3 inline-flex justify-center items-center gap-2 rounded-md font-semibold bg-[#49FBB5] text-gray-800 hover:bg-[#49FBB5]/80 focus:outline-none transition-all text-sm">
-            <svg
-              class="w-3 h-3"
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              viewBox="0 0 16 16"
-              fill="none"
-            >
-              <path
-                d="M2.63452 7.50001L13.6345 7.5M8.13452 13V2"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-              />
-            </svg>
-            Add task
-          </div>
+        <div className="grid grid-cols-2 gap-2">
+          <button
+            className={`cursor-pointer py-2 px-3 inline-flex justify-center items-center gap-2 rounded-md font-semibold  focus:outline-none transition-all text-sm ${
+              activePeriod === "month"
+                ? "bg-[#593EFE] text-gray-100 hover:bg-[#593EFE]/80"
+                : "bg-[#171717] text-gray-100 hover:bg-[#171717]/80"
+            }`}
+            onClick={() => setActivePeriod("month")}
+          >
+            Month
+          </button>
+          <button
+            className={`cursor-pointer py-2 px-3 inline-flex justify-center items-center gap-2 rounded-md font-semibold  focus:outline-none transition-all text-sm ${
+              activePeriod === "all"
+                ? "bg-[#593EFE] text-gray-100 hover:bg-[#593EFE]/80"
+                : "bg-[#171717] text-gray-100 hover:bg-[#171717]/80"
+            }`}
+            onClick={() => setActivePeriod("all")}
+          >
+            All
+          </button>
         </div>
       </div>
-      <div className="h-full bg-green-50"></div>
-      {/* <Chart
-        chartType="Bar"
-        width="100%"
-        height="100%"
-        data={data}
-        options={options}
-      /> */}
+      <div className="h-full bg-green-50">
+        <Chart
+          chartType="Bar"
+          width="100%"
+          height="100%"
+          data={data}
+          options={options}
+        />
+      </div>
     </div>
   );
 };
--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -7,12 +7,12 @@ import HelpCard from "./HelpCard";
 
 const Dashboard = ({ user, userTasks }) => {
   return (
-    <div className="h-full p-6 bg-[#0b0b0b] grid gap-10">
+    <div className="h-full p-6 bg-[#0b0b0b] gap-10 w-full">
       <div className="grid row-span-1 space-y-4">
         <p className="text-4xl text-white">Welcome {user.pseudo} 👋🏼</p>
         <StatsCard userTasks={userTasks} />
       </div>
-      <div className="grid row-span-3 gap-4 bg-red-200 h-[400px] md:grid-cols-3">
+      <div className="grid row-span-3 gap-4 h-[400px] md:grid-cols-3 w-full py-10">
         <div className="md:col-span-2">
           <Charts />
         </div>
